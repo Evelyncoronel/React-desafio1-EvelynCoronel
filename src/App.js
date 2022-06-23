1fec712ea0af5f871ec8c10da0b4a849a8fa1300
malbec <evelyn.ec.dc@gmail.com>
2022-06-22 21:38:40 -0300

    creacion de navbar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos a mi app! 
         </p>
         <a
           className="App-link"
---
+++ src/Caja.js
@@ -0,0 +1,13 @@
+function Caja() {
+    return (
+      <div className="Caja">
+        
+          <p>
+            Hola! Buenas noches {props.nombre}!
+          </p>
+         
+      </div>
+    );
+  }
+
+  export default Caja;
